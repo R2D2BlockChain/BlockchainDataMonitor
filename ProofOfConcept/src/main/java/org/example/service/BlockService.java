e46b79fd111e27a2ff2c9b948f2ce5103f545668
Maksymilian Gruszczyński <91141837+ynlamyskam@users.noreply.github.com>
2026-04-07 20:32:35 +0200

    Connection to the API, base class to use for retrieving data

--- ProofOfConcept/src/main/java/org/example/service/BlockService.java
+++ ProofOfConcept/src/main/java/org/example/service/BlockService.java
@@ -5,7 +5,6 @@ import org.web3j.protocol.core.methods.response.EthBlock;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BlockService {
@@ -16,32 +15,49 @@ public class BlockService {
         this.web3j = web3j;
     }
 
-    public List<EthBlock.Block> getLatestBlocks(int count) throws IOException, InterruptedException {
-        List<EthBlock.Block> blocks = new ArrayList<>();
-
-        BigInteger latestBlock = web3j.ethBlockNumber().send().getBlockNumber();
-
-
-        for (int i = 0; i < count; i++) {
-            BigInteger blockNumber = latestBlock.subtract(BigInteger.valueOf(i));
-
-
-            EthBlock blockResponse = web3j
-                    .ethGetBlockByNumber(
-                            org.web3j.protocol.core.DefaultBlockParameter.valueOf(blockNumber),
-                            true
-                    )
-                    .send();
-
-            EthBlock.Block block = blockResponse.getBlock();
-
-            if (block != null) {
-                blocks.add(block);
-            }
-
-            Thread.sleep(200);
-        }
-
-        return blocks;
+    public BigInteger getLatestBlock() throws IOException, InterruptedException {
+        return web3j.ethBlockNumber().send().getBlockNumber();
     }
+
+    public String getBlockNumber(EthBlock.Block block) throws IOException, InterruptedException {
+        return block.getNumber().toString();
+    }
+
+    public String getBlockHash(EthBlock.Block block) throws IOException, InterruptedException {
+        return block.getHash();
+    }
+
+    public String getBlockTransactionCount(EthBlock.Block block) throws IOException, InterruptedException {
+        return String.valueOf(block.getTransactions().size());
+    }
+
+    public List<EthBlock.TransactionResult> getTransactionsList(EthBlock.Block block) throws IOException, InterruptedException{
+        return block.getTransactions();
+    }
+
+    public EthBlock.TransactionObject getSingleTransaction(List<EthBlock.TransactionResult> transactionResults, int index) throws IOException, InterruptedException{
+        return (EthBlock.TransactionObject) transactionResults.get(index);
+    }
+
+    public String getSingleTransactionHash (EthBlock.TransactionObject transaction)  throws IOException, InterruptedException{
+        return transaction.getHash();
+    }
+
+    public String getSingleTransactionSender(EthBlock.TransactionObject transaction)  throws IOException, InterruptedException{
+        return transaction.getFrom();
+    }
+
+    public String getSingleTransactionReceiver(EthBlock.TransactionObject transaction)  throws IOException, InterruptedException{
+        return transaction.getTo();
+    }
+
+    public String getSingleTransactionValue(EthBlock.TransactionObject transaction)  throws IOException, InterruptedException{
+        return transaction.getValue().toString();
+    }
+
+    public String getSingleTransactionGas(EthBlock.TransactionObject transaction)  throws IOException, InterruptedException{
+        return transaction.getGas().toString();
+    }
+
+
 }
